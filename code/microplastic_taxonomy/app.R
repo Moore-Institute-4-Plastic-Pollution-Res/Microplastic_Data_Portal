20f814b735f6a1db7570da01c1cd2ca441aa02ff
Win <wincowger@gmail.com>
2022-08-30 17:26:20 -0700

    innitial app

---
+++ code/microplastic_taxonomy/app.R
@@ -0,0 +1,92 @@
+
+library(shiny)
+library(bs4Dash)
+library(googlesheets4)
+library(dplyr)
+
+gs4_deauth()
+file <- read_sheet("https://docs.google.com/spreadsheets/d/1pOLzGuweqyUinMaqYpWop0mqf0LaMNyHdCsWZ-ToV8M/edit?usp=sharing")
+file$images <- paste0("https://drive.google.com/uc?export=view&id=", gsub(".*id=", "", file$`Image File`))
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Microplastic Taxonomy"),
+    dashboardSidebar(
+        sidebarUserPanel(
+            #image = "https://drive.google.com/file/d/13iCjC10dV3giFhCCoir_8mnbwtHM1rMA/view?usp=sharing",
+            name = "Welcome Onboard!"
+        ),
+        
+        sidebarMenu(
+            id = "sidebarmenu",
+            #sidebarHeader("Header 1"),
+            menuItem(
+                "About",
+                tabName = "item1",
+                icon = icon("sliders")
+            ),
+            menuItem(
+                "Image Query",
+                tabName = "item2",
+                icon = icon("camera")
+            )
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(
+                tabName = "item1"
+            ),
+            tabItem(
+                tabName = "item2",
+                fluidRow(
+                    column(4, 
+                           selectInput(inputId = "color", 
+                                       label = "Color Selection", 
+                                       choices = c("All", "Black", "Blue", "White", "Green"),
+                                    )
+                           ),
+                    column(4,
+                           selectInput(inputId = "morphology", 
+                                       label = "Morphology Selection", 
+                                       choices = c("All", "Fiber", "Film", "Fragment", "Sphere"),
+                           )),
+                    column(4,
+                           selectInput(inputId = "polymer", 
+                                       label = "Polymer Selection", 
+                                       choices = c("All", "Polyethylene", "Polycarbonate", "Polyester", "Polyvinylchloride"),
+                           ))
+                ),
+                uiOutput("images")
+            )
+        )
+    )
+)
+
+server <- function(input, output) {
+    output$images <- renderUI({
+        #fluidRow(
+            boxLayout(
+                type = "group",
+            #sortable(
+            #    width = 4,
+                # p(class = "text-center", paste("Column", i)),
+                
+                lapply(file %>% 
+                           filter(if(input$color != "All") tolower(`Color of particle`) == tolower(input$color) else !is.na(images)) %>%
+                           filter(if(input$morphology != "All") tolower(`Morphology of particle`) == tolower(input$morphology) else !is.na(images)) %>%
+                           filter(if(input$polymer != "All") tolower(`Polymer-type of particle`) == tolower(input$polymer) else !is.na(images)) %>%
+                           
+                           pull(images), function(x){
+                    box(
+                        tags$image(src = x, height = "200rem"),
+                        width = NULL
+                    )
+                })
+            #)
+        )
+       # )
+    })
+
+}
+
+shinyApp(ui, server)
