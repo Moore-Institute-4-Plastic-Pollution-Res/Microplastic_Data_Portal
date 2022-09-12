ea5ca6ed6628f3ed16a34a4d1add326609ade361
wincowgerDEV <wincowger@gmail.com>
2022-09-12 15:58:29 -0700

    basic reading and uploading working fine for these small files.

--- code/ckanr.R
+++ code/ckanr.R
@@ -2,6 +2,8 @@
 #https://github.com/ropensci/ckanr
 
 library('ckanr')
+library(data.table)
+library(dplyr)
 
 api <- read.table("secrets/ckan.txt", sep = ",")
 ckanr_setup(url = "https://data.ca.gov/", key = api$V1)
@@ -21,29 +23,7 @@ activities <- ckanr::dashboard_activity_list()
 ckanr::dashboard_count()
 
 #Working, gathered from URL Under code. 
-file <- ckan_fetch("https://data.ca.gov/dataset/27b12123-c913-42c9-81f8-a7d1cbeb22d0/resource/707b9fff-441b-42ba-929e-38827e64880b/download/forbes-global-2000-2019.csv")
-
-# create a package not allowed with current permissions. 
-res <- package_create("test", author="wincowger")
-
-# then create a resource
-file <- system.file("examples", "actinidiaceae.csv", package = "ckanr")
-xx <- resource_create(package_id = res$id,
-                       description = "my resource",
-                       name = "bears",
-                       upload = file,
-                       extras = list(species = "grizzly"),
-                       rcurl = "http://google.com"
-)
-
-package_create("foobbbbbarrrr") %>%
-    resource_create(description = "my resource",
-                    name = "bearsareus",
-                    upload = file,
-                    extras = list(my_extra = "some value"),
-                    rcurl = "http://google.com")
-
-
+#Ckan does not like the word none, defaults to NA, may not always be what is intended. 
 #Richard Shared Code
 #library(ckanr) #write to portal#### # get the data portal API key saved in the local environment (it's available on data.ca.gov by going to your user profile) 
 #portal_key <- Sys.getenv('portal_key') # set the ckan defaults 
@@ -51,5 +31,26 @@ resourceID <- '707b9fff-441b-42ba-929e-38827e64880b'
 ckan_resource_info <- resource_show(id = resourceID, as = 'table')
 path <- system.file("examples", "actinidiaceae.csv", package = "ckanr")
 file_upload <- ckanr::resource_update(id = resourceID, path = path)
-#Will update the file and tell you that it has actually been updated with the assignment. 
+file <- ckan_fetch(ckan_resource_info$url)
+testfile <- read.csv(path) #Periods get parsed as spaces in ckan. 
+#ckan_resource_info$last_modified
 
+#Will update the file and tell you that it has actually been updated with the assignment. 
+ckan <- src_ckan("https://data.ca.gov/")
+
+testdplyr <- dplyr::tbl(src = ckan$con, from = resourceID) %>% 
+    select(Family) # %>% 
+    #filter(HBT2014 == "S08000015") %>% 
+    as_tibble()
+
+
+#Works pretty well for data download with filters. 
+test <- ds_search(resource_id = resourceID, filters = '{"Confidence level": "M"}', as = "table", limit = 100000)
+records <- test$records
+
+?ds_search()
+
+ds_create(resource_id = resourceID)
+# Could go with this in the long run when the dataset starts getting really large. ?ckanr::ds_create()
+# Also a minimal example of something a little more advanced here: https://github.com/ropensci/ckanr/pull/102
+# Another useful strategy https://www.r-bloggers.com/2019/11/trying-the-ckanr-package/ 
