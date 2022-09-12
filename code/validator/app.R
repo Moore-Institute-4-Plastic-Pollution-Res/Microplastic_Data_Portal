d26d6df860b862bf02f8c17190b62fcdb345a563
wincowgerDEV <wincowger@gmail.com>
2022-09-12 16:32:17 -0700

    basic interaction working.

--- code/validator/app.R
+++ code/validator/app.R
@@ -130,48 +130,194 @@ ui <- dashboardPage(
     )
 )
 
-
-
-server <- function(input, output) {
+server <- function(input, output, session) {
     
-    filtered <- reactive({
-        filtered_test <- file %>% 
-            filter(if(input$color != "ALL") tolower(`Color of particle`) == tolower(input$color) else !is.na(images)) %>%
-            filter(if(input$morphology != "ALL") tolower(`Morphology of particle`) == tolower(input$morphology) else !is.na(images)) %>%
-            filter(if(input$polymer != "ALL") tolower(`Polymer-type of particle`) == tolower(input$polymer) else !is.na(images))
-        
-        if(nrow(filtered_test) == 0){
-            NULL
+    rules_example <- read.csv("www/rules.csv") 
+    
+    data_example <- read.csv("www/Samples.csv")
+    
+    dataset <- reactiveValues(data = NULL)
+    
+    validation_summary <- reactiveValues(results = NULL, report = NULL, rules = NULL)
+    
+    observeEvent(input$file_rules, {
+        file_rules <- input$file_rules$datapath
+        if (!grepl("(\\.csv$)",
+                   ignore.case = T, as.character(file_rules))) {
+            #reset("file")
+            dataset$data <- NULL
+            validation_summary$rules <- NULL
+            validation_summary$report <- NULL
+            validation_summary$results <- NULL
+            show_alert(
+                title = "Data type not supported!",
+                text = paste0("Uploaded data type is not currently supported; please
+                      upload a .csv file."),
+                type = "warning")
+            #return(NULL)
         }
         else{
-            filtered_test %>%
-                slice_sample(n= if(nrow(.) > 100) 100 else nrow(.))
-        }
-    })    
-    output$images <- renderUI({
-        req(filtered())
-        boxLayout(
-            type = "group",
-            #sortable(
-            #    width = 4,
-            # p(class = "text-center", paste("Column", i)),
+            rules <- read.csv(input$file_rules$datapath)
             
-            lapply(1:nrow(filtered()), function(x){
-                box(
-                    title = filtered()$`Researcher Name`[x],
-                    
-                    tags$figure(tags$img(src = filtered()$images[x], width = "400em"),
-                                tags$figcaption(tags$small(filtered()$`Citation`[x]))),
-                    maximizable = T,
-                    width = NULL
-                )
-            })
-            #)
-        )
+            validation_summary$rules <- validator(.data=rules)
+        }
         
-        # )
     })
     
+    observeEvent(input$file, {
+        req(input$file)
+        file <- input$file$datapath
+        
+        # Read in data when uploaded based on the file type
+        if (!grepl("(\\.csv$)", ignore.case = T, as.character(file))) {
+            dataset$data <- NULL
+            validation_summary$rules <- NULL
+            validation_summary$report <- NULL
+            validation_summary$results <- NULL
+            show_alert(
+                title = "Data type not supported!",
+                text = paste0("Uploaded data type is not currently supported; please
+                      upload a .csv file."),
+                type = "warning")
+        }
+        else if (is.null(input$file_rules)) {
+            #reset("file")
+            dataset$data <- NULL
+            validation_summary$rules <- NULL
+            validation_summary$report <- NULL
+            validation_summary$results <- NULL
+            show_alert(
+                title = "Need Rules File",
+                text = paste0("You must upload a rules file before uploading a data file to validate."),
+                type = "warning")
+            #return(NULL)
+        }
+        else{
+            dataset$data <- read.csv(file)
+            
+            validation_summary$report <- confront(dataset$data, validation_summary$rules)
+            
+            validation_summary$results <- summary(validation_summary$report) %>%
+                mutate(status = ifelse(fails > 0 | error | warning , "error", "success")) %>%
+                mutate(description = meta(validation_summary$rules)$description)
+            
+        }
+        
+    })
+    
+    overview_table <- reactive({
+        req(input$file)
+        validation_summary$results %>%
+            filter(if(input$show_decision){status == "error"} else{status %in% c("error", "success")}) %>%
+            select(description, status, name, expression, everything())
+    })
+    
+    selected <- reactive({
+        req(input$file)
+        req(input$show_report_rows_selected)
+        violating(dataset$data, validation_summary$report[overview_table()[input$show_report_rows_selected, "name"]])
+    })
+    
+    
+    output$certificate <- renderUI({
+        req(file)
+        req(validation_summary$results)
+        
+        if(all(validation_summary$results$status != "error")){
+            downloadButton("download_certificate", "Download Certificate", style = "background-color: #2a9fd6; width: 100%;")
+        }
+        else{
+            NULL
+        }
+    })
+    
+    
+    output$alert <- renderUI({
+        req(input$file)
+        req(validation_summary$results)
+        if(any(validation_summary$results$status == "error")){
+            HTML('<button type="button" class="btn btn-danger btn-lg btn-block">ERROR</button>')
+        }
+        else{
+            HTML('<button type="button" class="btn btn-success btn-lg btn-block">SUCCESS</button>')
+        }
+    })
+    
+    
+    output$show_report <- DT::renderDataTable({
+        req(input$file)
+        req(any(validation_summary$results$status == "error"))
+        datatable({overview_table() %>%
+                select(description, status)},
+                extensions = 'Buttons',
+                options = list(
+                    searchHighlight = TRUE,
+                    scrollX = TRUE,
+                    lengthChange = FALSE, 
+                    pageLength = 5,
+                    paging = TRUE,
+                    searching = TRUE,
+                    fixedColumns = TRUE,
+                    autoWidth = TRUE,
+                    ordering = TRUE,
+                    dom = 'Bfrtip',
+                    buttons = c('copy', 'csv', 'excel', 'pdf')),
+                rownames = FALSE,
+                filter = "top", 
+                style = "bootstrap", 
+                selection = list(mode = "single", color = "red", selected = c(1))) %>%
+            formatStyle(
+                'status',
+                target = 'row',
+                backgroundColor = styleEqual(c("error", "success"), c('red', 'green')))
+    })
+    
+    output$report_selected <- DT::renderDataTable({
+        req(input$file)
+        req(input$show_report_rows_selected)
+        req(any(validation_summary$results$status == "error"))
+        datatable({selected()},
+                  rownames = FALSE,
+                  filter = "top", 
+                  extensions = 'Buttons',
+                  options = list(
+                      searchHighlight = TRUE,
+                      scrollX = TRUE,
+                      lengthChange = FALSE, 
+                      pageLength = 5,
+                      paging = TRUE,
+                      searching = TRUE,
+                      fixedColumns = TRUE,
+                      autoWidth = TRUE,
+                      ordering = TRUE,
+                      dom = 'Bfrtip',
+                      buttons = c('copy', 'csv', 'excel', 'pdf')),
+                  class = "display",
+                  style = "bootstrap") %>% 
+            formatStyle(
+                if(any(validation_summary$results$status == "error")){
+                    variables(validation_summary$rules[overview_table()[input$show_report_rows_selected, "name"]])  
+                }
+                else{NULL},
+                backgroundColor =  'red'
+            )
+    })
+    
+    
+    #Downloads ----
+    output$download_certificate <- downloadHandler(
+        filename = function() {"certificate.csv"},
+        content = function(file) {write.csv(data.frame(time = Sys.time(), data = digest(dataset$data), rules = digest(validation_summary$rules), package_version = packageVersion("validate"), web_hash = digest(paste(sessionInfo(), Sys.time(), Sys.info()))), file, row.names = F)}
+    )
+    output$download_rules <- downloadHandler( 
+        
+        filename = function() {"rules.csv"},
+        content = function(file) {write.csv(rules_example, file, row.names = F)}
+    )
+    output$download_sample <- downloadHandler(
+        filename = function() {"data.csv"},
+        content = function(file) {write.csv(data_example, file, row.names = F)}
+    )
 }
 
 shinyApp(ui, server)
