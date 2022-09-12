677aface209229bcba7749fc143aa342586ea803
wincowgerDEV <wincowger@gmail.com>
2022-09-12 14:29:04 -0700

    working CKAN download, upload, crud.

--- code/ckanr.R
+++ code/ckanr.R
@@ -3,22 +3,25 @@
 
 library('ckanr')
 
-api <- read.table("secrets/ckan.txt")
+api <- read.table("secrets/ckan.txt", sep = ",")
 ckanr_setup(url = "https://data.ca.gov/", key = api$V1)
+#ckanr_setup(url = "[https://data.ca.gov/](https://data.ca.gov/)", key = api$V1) 
+
 
 #Current info on the server
 ckan_info()
 ckanr::ckan_version()
-changes(limit = 2, as = "table")[, 1:4]
+changes(limit = 2, as = "table")
 ckanr::ckanr_settings()
 package_list(as = "table")
 tag_list('aviation', as = 'table')
 
 #Check your info
-ckanr::dashboard_activity_list()
+activities <- ckanr::dashboard_activity_list()
 ckanr::dashboard_count()
 
-file <- ckan_fetch("https://data.cnra.ca.gov/dataset/78ac95ac-9665-4e53-aea6-ad0762055cce/resource/4e6aaf6f-7055-484b-84f4-3a66df294f27/download/2020-09-11_microparticledata.xlsx")
+#Working, gathered from URL Under code. 
+file <- ckan_fetch("https://data.ca.gov/dataset/27b12123-c913-42c9-81f8-a7d1cbeb22d0/resource/707b9fff-441b-42ba-929e-38827e64880b/download/forbes-global-2000-2019.csv")
 
 # create a package not allowed with current permissions. 
 res <- package_create("test", author="wincowger")
@@ -42,11 +45,11 @@ package_create("foobbbbbarrrr") %>%
 
 
 #Richard Shared Code
-library(ckanr) #write to portal#### # get the data portal API key saved in the local environment (it's available on data.ca.gov by going to your user profile) 
-portal_key <- Sys.getenv('portal_key') # set the ckan defaults 
-ckanr_setup(url = '[https://data.ca.gov/](https://data.ca.gov/)', key = portal_key) 
-resourceID <- 'XXXXX' 
+#library(ckanr) #write to portal#### # get the data portal API key saved in the local environment (it's available on data.ca.gov by going to your user profile) 
+#portal_key <- Sys.getenv('portal_key') # set the ckan defaults 
+resourceID <- '707b9fff-441b-42ba-929e-38827e64880b' 
 ckan_resource_info <- resource_show(id = resourceID, as = 'table')
-file_upload <- ckanr::resource_update(id = resourceID, path = paste0("files to upload/XXXXX", Sys.Date(),'.csv'))
-
+path <- system.file("examples", "actinidiaceae.csv", package = "ckanr")
+file_upload <- ckanr::resource_update(id = resourceID, path = path)
+#Will update the file and tell you that it has actually been updated with the assignment. 
 
