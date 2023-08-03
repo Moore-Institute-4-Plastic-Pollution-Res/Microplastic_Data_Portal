737f81c62c48a0f4129f0136648ca769c4300a6c
hsherrod2019 <hsherrod2019@gmail.com>
2023-08-03 09:35:12 -0700

    Added a zip file + updated rules/data1/data2

--- code/validator/www/microplastic_images/zipfilecode.R
+++
@@ -1,5 +0,0 @@
-library(utils)
-image_files <- c("code/validator/www/microplastic_images/MicroplasticImages/ALGALITA_CW_3_above500_30.jpeg", "code/validator/www/microplastic_images/MicroplasticImages/B_DW_3_above500_96.jpeg", "code/validator/www/microplastic_images/MicroplasticImages/CC_CW_1_20-250_176.jpeg")
-MicroplasticImages <- "MicroplasticImages.zip"
-zip(MicroplasticImages, files = image_files)
-
